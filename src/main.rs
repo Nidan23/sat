bd395cf218e30f829c6529c11b0079a1ca93283f
Nidan23 <dev.sulkowski@gmail.com>
2022-09-04 19:59:49 +0200

    [#1][ADD] Added imports / main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+extern crate core;
+
+mod controller;
+mod service;
+mod model;
+
+use actix_web::{App, HttpServer};
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(controller::car_controller::calculate_diesel_usage_for_distance)
+            .service(controller::car_controller::probability_of_unit_injector_fail)
+    })
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
 }
